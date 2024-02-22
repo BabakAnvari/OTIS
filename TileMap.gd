efbd831e01486426272a4964cb800e2525a8dc78
Juliansss <90258003+Juliansss@users.noreply.github.com>
2024-02-22 13:36:42 +0100

    Fixed read_tile function and added loop

--- TileMap.gd
+++ TileMap.gd
@@ -2,38 +2,22 @@ extends TileMap
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	var tile_properties_array = read_all_tiles()
+	var tile_properties = []
 
-	# Now you can iterate over the array and use the properties
-	for tile_properties in tile_properties_array:
-		var x = tile_properties[0].x
-		var y = tile_properties[0].y
-		var block_id = tile_properties[1]
+	for x in range(get_used_rect().size.x):
+		for y in range(get_used_rect().size.y):
+			var tile_data = TileData.new()  # Assuming TileData is a class you've defined
+			tile_data.texture_origin = Vector2(x, y)  # Update the tile_data with the current coordinates
 
-		print(x, y, block_id)
+			var tile_info = read_tile(tile_data)
+			
+			tile_properties.append(tile_info)
 
+	print(tile_properties)
+	
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
-
-func read_all_tiles() -> Array:
-	var tile_properties_array = []
-
-	# Get the tile size from the TileMap node in the scene
-	var tile_size = $TileMap.cell_size
-
-	# Iterate over all tiles in the TileMap
-	for x in range($TileMap.get_used_rect().size.x):
-		for y in range($TileMap.get_used_rect().size.y):
-			var world_position = $TileMap.map_to_world(Vector2(x, y) * tile_size)
-			var tile_coordinates = $TileMap.world_to_map(world_position)
-
-			if $TileMap.get_cell(x, y) != -1:
-				var tile_data = $TileMap.tile_set.tile_get_tile_data($TileMap.get_cell(x, y))
-				var tile_properties = read_tile(tile_data)
-				tile_properties_array.append(tile_properties)
-
-	return tile_properties_array
 
 func read_tile(tile_data: TileData) -> Array:
 	var tile_properties = []
@@ -43,9 +27,7 @@ func read_tile(tile_data: TileData) -> Array:
 	tile_properties.append(tile_coordinates)
 
 	# Block ID
-	var block_id = tile_data.terrain_set  # You may modify this based on your requirements
-	tile_properties.append(block_id)
-
-	print(tile_coordinates, ", ", block_id)
+	var block_id = tile_data.terrain_set
+	tile_properties.append(block_id) 
 
 	return tile_properties
